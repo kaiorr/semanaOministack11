dc18bdc5589ee47374ce5cce0e395d6347aa1f42
kaiorr <kaio.rocharibeiro@gmail.com>
2020-03-26 07:22:43 -0300

    feat(updated_at): criado rota de login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const ongController = require('./controllers/ongController');
 const incidentController = require('./controllers/incidentController');
 const profileController = require('./controllers/profileController');
+const sessionController = require('./controllers/sessionController');
+
 const routes = express.Router();
+
+routes.post('/sessions', sessionController.create);
 
 routes.get('/ongs', ongController.index);
 routes.post('/ongs', ongController.create);
