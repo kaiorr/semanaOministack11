adeb64a6828dbd2128dcf1bea33dd0ff23ab4344
kaiorr <kaio.rocharibeiro@gmail.com>
2020-03-26 06:36:38 -0300

    feat(updated_at): deifinido o controller e rota profile
    	profileController: Definido o controller que contém a lógica de apresentação de casos por determinada ong_id, ou seja somente será apresentado os casos de determinada ONG e não todos os casos de
    todas as ONGś.
    
    	rota de profile: Definida a rota que faz a lógica do controller acima, ser apresentada.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const ongController = require('./controllers/ongController');
 const incidentController = require('./controllers/incidentController');
+const profileController = require('./controllers/profileController');
 const routes = express.Router();
 
 routes.get('/ongs', ongController.index);
 routes.post('/ongs', ongController.create);
 
+routes.get('/profile', profileController.index);
+
+
 routes.get('/incidents', incidentController.index);
 routes.post('/incidents', incidentController.create);
-
 routes.delete('/incidents/:id', incidentController.delete);
 
 module.exports = routes;
