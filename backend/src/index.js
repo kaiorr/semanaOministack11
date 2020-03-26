cde57d90af6b3e7e5d85a976506f18e152a0700a
kaiorr <kaio.rocharibeiro@gmail.com>
2020-03-26 07:45:44 -0300

    feat: adicionado o móculo de cors
    	O módulo de cors é adicionado para que requisição pelo frontend seja feita somente onde o nossa aplicação estiver hospedada, como estamos em ambiente DEV, não foi adicionado nenuma origin.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require('cors');
 const routes = require("./routes");
 
 const app = express();
+app.use(cors());
 app.use(express.json()); //para converter json no corpo da requisição para javascript
 app.use(routes);
 app.listen(3333); // porta utilizada pelo servidor
