e69fd20739faab73bcc9a61cb1e5de25553ef1d9
kaiorr <kaio.rocharibeiro@gmail.com>
2020-03-27 08:05:56 -0300

    refactor: refeito identação da class logon-container

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
-
 import './styles.css';
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+import {FiLogIn} from 'react-icons/fi';
 
 export default function Logon() {
     return (
-        
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero" />
+
+                <form>
+                
+                    <h1>Faça seu Logon</h1>
+                    <input placeholder="Informe seu ID" />
+                    <button className="button" type="submit">Entrar</button>
+
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041"/>
+                        Não tenho Cadastro
+                    </a>
+                
+                </form>
+
+            </section>
+            <img src={heroesImg} alt="heroes" />
+        </div>
     );
 }
