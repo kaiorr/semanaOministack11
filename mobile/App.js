c76622bc7b9d2cabc8057ce74bdfb486c9d91e75
kaiorr <kaio.rocharibeiro@gmail.com>
2020-03-29 10:56:36 -0300

    fix: correção de arquivos e paginação no mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,7 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+
+import Routes from "./src/routes";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>To com fome, faz um Bauru ai pra nois. Bjus!
-        
-      </Text>
-    </View>
-  );
+  return <Routes />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
